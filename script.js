8efa35909a47d242473da20cf27f9307f9bc9364
MarcellinoAcel <saragihmarcel34@gmail.com>
2024-10-22 06:37:26 +0700

    update

--- script.js
+++ script.js
@@ -11,12 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
-
-// Thumbnail image controls
+ 
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -29,27 +27,22 @@ function showSlides(n) {
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
 
-    // Hide all slides
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
-    // Remove the "active" class from all dots
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    // Display the current slide and activate the corresponding dot
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
 
-// Automatic gallery
 let slideInterval = setInterval(function () {
     plusSlides(1);
-}, 10000); // 5 seconds interval
+}, 10000);
 
-// Automatic Slideshow - change image every 4 seconds
 var myIndex = 0;
 carousel();
 
@@ -65,7 +58,6 @@ function carousel() {
     setTimeout(carousel, 4000);
 }
 
-// Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
@@ -75,7 +67,6 @@ function myFunction() {
     }
 }
 
-// When the user clicks anywhere outside of the modal, close it
 var modal = document.getElementById('ticketModal');
 window.onclick = function (event) {
     if (event.target == modal) {
