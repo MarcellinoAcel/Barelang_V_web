c7e4c14ff666310c7815b40bb5a75189432685a2
MarcellinoAcel <saragihmarcel34@gmail.com>
2024-10-22 11:58:19 +0700

    update daffa

--- script.js
+++ script.js
@@ -58,20 +58,4 @@ function carousel() {
     setTimeout(carousel, 4000);
 }
 
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
-var modal = document.getElementById('ticketModal');
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 
