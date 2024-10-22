6e287e5144feb3e45e5b11d1dfdde94bbc11eb4e
MarcellinoAcel <saragihmarcel34@gmail.com>
2024-10-22 13:09:38 +0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('.gallery-container img');
-
-    images.forEach((img) => {
-        img.addEventListener('click', function () {
-            alert(`Anda mengklik gambar: ${this.alt}`);
-        });
-    });
-});
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -42,20 +32,3 @@ function showSlides(n) {
 let slideInterval = setInterval(function () {
     plusSlides(1);
 }, 10000);
-
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) { myIndex = 1 }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 4000);
-}
-
-
